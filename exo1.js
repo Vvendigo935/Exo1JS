17d254a8280e6b48ccb8cf897b71d35d69790f78
vivien <vivien@m2i.fr>
2025-07-21 15:02:28 +0200

    exo 1 js avec bonus

--- exo1.js
+++ exo1.js
@@ -1,25 +1,25 @@
-let scoresJoueur = [];
+const scoresJoueur = [];
 
 scoresJoueur.push(78, 95, 48, 63, 85);
 
 console.table(scoresJoueur);
 
-let note1 = scoresJoueur[0];
-let note2 = scoresJoueur[1];
-let note3 = scoresJoueur[2];
-let note4 = scoresJoueur[3];
-let note5 = scoresJoueur[4];
+const note1 = scoresJoueur[0];
+const note2 = scoresJoueur[1];
+const note3 = scoresJoueur[2];
+const note4 = scoresJoueur[3];
+const note5 = scoresJoueur[4];
 
-function moyenne(note1, note2, note3, note4, note5, result){
-    return result = note1 + note2 + note3 + note4 + note5;
-}
-let result = note1 + note2 + note3 + note4 + note5;
+//function moyenne(note1, note2, note3, note4, note5, result){
+//    return result = note1 + note2 + note3 + note4 + note5;
+//}
+const result = note1 + note2 + note3 + note4 + note5;
 
 // let somme = moyenne(scoresJoueur.forEach);
 
 // let moyenneOk = somme / scoresJoueur.length;
 
-let moy = result/scoresJoueur.length;
+const moy = result/scoresJoueur.length;
 
 console.log(moy); // afficher la moyenne
 
@@ -49,3 +49,48 @@ for (let i = 0; i < scoresJoueur.length; i++) {
     console.log("Eleve numero " + i + " : excellent")
     
    }
+
+   let insuf = 0;
+   let passable = 0;
+   let bien = 0;
+   let tresbien = 0;
+   let excellent = 0;
+
+
+
+   function nombreMentions(scores) {
+
+    for (let i = 0; i < scoresJoueur.length; i++) {
+
+    
+        if(scoresJoueur[i] < 50){
+            insuf++;
+            
+        } else if (scoresJoueur[i] < 64){
+            passable++;
+            
+        } else if (scoresJoueur[i] < 79){
+            bien++;
+            
+        } else if (scoresJoueur[i] < 89){
+            tresbien++;
+            
+        } else
+        excellent++;
+        
+        
+       }
+
+        const mentionTab = [];
+        mentionTab.push(insuf,passable,bien,tresbien,excellent);
+        console.log("nombre d'eleve ayant eu la mention insuffisante : " + insuf);
+        console.log("nombre d'eleve ayant eu la mention passable : " + passable);
+        console.log("nombre d'eleve ayant eu la mention bien : " + bien);
+        console.log("nombre d'eleve ayant eu la mention tres bien : " + tresbien);
+        console.log("nombre d'eleve ayant eu la mention excellent : " + excellent);
+
+       
+        console.log(mentionTab);
+   }
+  
+   nombreMentions(scoresJoueur);
